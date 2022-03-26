54992db34048d38c32674c5421f9cdb836e6bb70
brightseon <public.seon@gmail.com>
2022-03-26 11:46:39 +0900

    3.1 TailwindCSS Setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    experimental: {
+        reactRoot: 'concurrent'
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
