fe6e698574a578177438377d2bc20e6db248c457
brightseon <public.seon@gmail.com>
2022-05-14 09:57:07 +0900

    5.14 Stream

--- pages/live/[id].tsx
+++ pages/live/[id].tsx
@@ -4,19 +4,6 @@ const LiveDetail: NextPage = () => {
     return (
         <div className="py-10 px-4 space-y-4">
             <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
-            <div className="mt-5">
-                <h1 className="text-3xl font-bold text-gray-900">Galaxy S50</h1>
-                <span className="text-2xl block mt-3 text-gray-900">$140</span>
-                <p className=" my-6 text-gray-700">
-                    My money&apos;s in that office, right? If she start giving me some bullshit
-                    about it ain&apos;t there, and we got to go someplace else and get it, I&apos;m
-                    gonna shoot you in the head then and there. Then I&apos;m gonna shoot that bitch
-                    in the kneecaps, find out where my goddamn money is. She gonna tell me too. Hey,
-                    look at me when I&apos;m talking to you, motherfucker. You listen: we go in
-                    there, and that ni**a Winston or anybody else is in there, you the first
-                    motherfucker to get shot. You understand?
-                </p>
-            </div>
             <h3 className="text-gray-800 font-semibold text-2xl mt-2">Let&apos;s try potatos</h3>
             <div className="py-10 pb-16 h-[50vh] overflow-y-scroll px-4 space-y-4">
                 <div className="flex items-start space-x-2">
