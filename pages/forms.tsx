9c0cb51f5d9817382922aea62d08c577ea9136f6
brightseon <public.seon@gmail.com>
2022-06-01 22:50:13 +0900

    7.5 Errors

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,7 +1,6 @@
 import { FieldErrors, useForm } from 'react-hook-form';
 
-// Better validation
-// Better Errors (set, clear, display)
+// Better Errors (set, clear)
 // Have control over inputs
 
 interface LoginForm {
@@ -11,13 +10,23 @@ interface LoginForm {
 }
 
 export default function Forms() {
-    const { register, handleSubmit } = useForm<LoginForm>();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<LoginForm>({
+        mode: 'onChange'
+    });
 
     const onValid = (data: LoginForm) => {
         console.log('im valid bby');
     };
 
-    const onInvalid = (error: FieldErrors) => {};
+    const onInvalid = (error: FieldErrors) => {
+        console.log(error);
+    };
+
+    console.log(errors);
 
     return (
         <form onSubmit={handleSubmit(onValid, onInvalid)}>
@@ -30,10 +39,17 @@ export default function Forms() {
                 placeholder="Username"
             />
             <input
-                {...register('email', { required: 'Email is required' })}
+                {...register('email', {
+                    required: 'Email is required',
+                    validate: {
+                        notGmail: (value) => !value.includes('@gmail.com') || 'Gmail is not allowed'
+                    }
+                })}
                 type={'email'}
                 placeholder="Email"
+                className={`${Boolean(errors.email?.message) ? 'border-red-500' : ''}`}
             />
+            {errors.email?.message}
             <input
                 {...register('password', { required: 'Password is required' })}
                 type={'password'}
