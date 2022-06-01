65034ebf188b1c1dd5f7893b40bb47435bf3c4c8
brightseon <public.seon@gmail.com>
2022-06-01 18:13:26 +0900

    7.2 The Register Function

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,73 +1,20 @@
-import { SyntheticEvent, useState } from 'react';
+import { useForm } from 'react-hook-form';
+
+// Less code (c)
+// Better validation
+// Better Errors (set, clear, display)
+// Have control over inputs
+// Dont deal with events (c)
+// Easier Inputs (c)
 
 export default function Forms() {
-    const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [formErrors, setFormErrors] = useState('');
-    const [emailError, setEmailError] = useState('');
-
-    const onUsernameChange = (event: SyntheticEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value }
-        } = event;
-
-        setUsername(value);
-    };
-
-    const onEmailChange = (event: SyntheticEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value }
-        } = event;
-        setEmailError('');
-        setEmail(value);
-    };
-
-    const onPasswordChange = (event: SyntheticEvent<HTMLInputElement>) => {
-        const {
-            currentTarget: { value }
-        } = event;
-
-        setPassword(value);
-    };
-
-    const onSubmit = (event: SyntheticEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(email, username, password);
-        if (username === '' || email === '' || password === '') {
-            setFormErrors('All fields are required');
-        }
-
-        if (!email.includes('@')) {
-            setFormErrors('email is required');
-        }
-    };
+    const { register, watch } = useForm();
 
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                value={username}
-                onChange={onUsernameChange}
-                type={'text'}
-                placeholder="Username"
-                required
-                minLength={5}
-            />
-            <input
-                value={email}
-                onChange={onEmailChange}
-                type={'email'}
-                placeholder="Email"
-                required
-            />
-            {emailError}
-            <input
-                value={password}
-                onChange={onPasswordChange}
-                type={'password'}
-                placeholder="Password"
-                required
-            />
+        <form>
+            <input {...register('username')} type={'text'} placeholder="Username" required />
+            <input {...register('email')} type={'email'} placeholder="Email" required />
+            <input {...register('password')} type={'password'} placeholder="Password" required />
             <input type={'submit'} value="Create Account" />
         </form>
     );
