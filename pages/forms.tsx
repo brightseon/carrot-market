b2f8da2586ae74e58447f6058eede2911d07c4b3
brightseon <public.seon@gmail.com>
2022-06-01 18:30:02 +0900

    7.3 Validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -8,13 +8,25 @@ import { useForm } from 'react-hook-form';
 // Easier Inputs (c)
 
 export default function Forms() {
-    const { register, watch } = useForm();
+    const { register, handleSubmit } = useForm();
+
+    const onValid = () => {
+        console.log('im valid bby');
+    };
 
     return (
-        <form>
-            <input {...register('username')} type={'text'} placeholder="Username" required />
-            <input {...register('email')} type={'email'} placeholder="Email" required />
-            <input {...register('password')} type={'password'} placeholder="Password" required />
+        <form onSubmit={handleSubmit(onValid)}>
+            <input
+                {...register('username', { required: true })}
+                type={'text'}
+                placeholder="Username"
+            />
+            <input {...register('email', { required: true })} type={'email'} placeholder="Email" />
+            <input
+                {...register('password', { required: true })}
+                type={'password'}
+                placeholder="Password"
+            />
             <input type={'submit'} value="Create Account" />
         </form>
     );
