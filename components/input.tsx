2ff3244ae6d47ca24848b645fa1dfe58f13e5bf8
brightseon <public.seon@gmail.com>
2022-06-02 22:20:03 +0900

    8.0 Enter Form

--- components/input.tsx
+++ components/input.tsx
@@ -1,11 +1,22 @@
+import type { UseFormRegisterReturn } from 'react-hook-form';
+
 interface InputProps {
     label: string;
     name: string;
     kind?: 'text' | 'phone' | 'price';
-    [key: string]: any;
+    type: string;
+    register: UseFormRegisterReturn;
+    required: boolean;
 }
 
-export default function Input({ label, name, kind = 'text', ...rest }: InputProps) {
+export default function Input({
+    label,
+    name,
+    kind = 'text',
+    register,
+    type,
+    required
+}: InputProps) {
     return (
         <div>
             <label className="mb-1 block text-sm font-medium text-gray-700" htmlFor={name}>
@@ -15,7 +26,9 @@ export default function Input({ label, name, kind = 'text', ...rest }: InputProp
                 <div className="rounded-md relative flex  items-center shadow-sm">
                     <input
                         id={name}
-                        {...rest}
+                        required={required}
+                        {...register}
+                        type={type}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
@@ -27,7 +40,9 @@ export default function Input({ label, name, kind = 'text', ...rest }: InputProp
                     </div>
                     <input
                         id={name}
-                        {...rest}
+                        required={required}
+                        {...register}
+                        type={type}
                         className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                     <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
@@ -42,7 +57,9 @@ export default function Input({ label, name, kind = 'text', ...rest }: InputProp
                     </span>
                     <input
                         id={name}
-                        {...rest}
+                        required={required}
+                        {...register}
+                        type={type}
                         className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                     />
                 </div>
