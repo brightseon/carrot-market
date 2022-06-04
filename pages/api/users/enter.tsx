524270375b980fc4d0cf45c8bd4166f0ce8009c0
brightseon <public.seon@gmail.com>
2022-06-04 21:09:28 +0900

    8.5 Paths

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler from '../../../libs/server/withHandler';
+import withHandler from '@libs/server/withHandler';
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
     console.log(req.body);
