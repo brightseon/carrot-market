889cd1f87d3536ad3156ad306c518b4429b462b7
brightseon <public.seon@gmail.com>
2022-06-22 23:08:28 +0900

    9.3 Twilo Setup

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,19 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import withHandler from '@libs/server/withHandler';
+import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
     const { phone, email } = req.body;
-    const payload = phone ? { phone: +phone } : { email };
+    const user = phone ? { phone: +phone } : email ? { email } : null;
+
+    if (!user) return res.status(400).json({ ok: false });
+
+    const payload = `${Math.floor(100000 + Math.random() * 900000)}`;
     const token = await client.token.create({
         data: {
-            payload: '1234',
+            payload,
             user: {
                 connectOrCreate: {
-                    where: { ...payload },
+                    where: { ...user },
                     create: {
                         name: 'Anonymous',
-                        ...payload
+                        ...user
                     }
                 }
             }
@@ -42,8 +46,8 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
 
     //     console.log(user);
     // }
-    console.log(token);
-    return res.status(200).end();
+
+    return res.json({ ok: true });
 }
 
 export default withHandler('POST', handler);
