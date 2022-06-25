5b9bb8b8441affbdf29fe817b8ee72ecaff8bb52
brightseon <public.seon@gmail.com>
2022-06-25 19:53:14 +0900

    9.4 Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import withHandler, { ResponseType } from '@libs/server/withHandler';
 import client from '@libs/server/client';
+import twilio from 'twilio';
+
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) {
     const { phone, email } = req.body;
@@ -23,6 +26,15 @@ async function handler(req: NextApiRequest, res: NextApiResponse<ResponseType>) 
             }
         }
     });
+
+    if (phone) {
+        const message = await twilioClient.messages.create({
+            messagingServiceSid: process.env.TWILIO_MSID,
+            to: process.env.MY_PHONE!,
+            body: `Your login token is ${payload}.`
+        });
+        console.log(message);
+    }
     // if (email) {
     //     user = await client.user.findUnique({ where: { email } });
 
